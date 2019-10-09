06767c78260c0e07a0c61823cebcfbad4cc8ce80
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-09 17:09:31 +0800

    вращение

--- app/src/main/java/com/foxy/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/foxy/draganddraw/BoxDrawingView.java
@@ -22,6 +22,9 @@ public class BoxDrawingView extends View {
     private Paint boxPaint;
     private Paint backgroundPaint;
 
+    private float rotate;
+    private int currentIndex;
+
     // используется при создании view в коде
     public BoxDrawingView(Context context) {
         super(context);
@@ -41,6 +44,51 @@ public class BoxDrawingView extends View {
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:                  // первое касание
+                action = "ACTION_DOWN";
+
+                // сброс текущего состояния
+                currentBox = new Box(current);
+                boxen.add(currentBox);
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:         // последующие косания
+                currentIndex = event.getActionIndex();
+                break;
+            case MotionEvent.ACTION_MOVE:                // любое движение
+                action = "ACTION_MOVE";
+                if (currentBox != null) {
+                    if (currentIndex != 0 && currentIndex == event.getActionIndex()) {
+                        rotate = event.getHistoricalY(currentIndex, 1);
+                        current.set(event.getX() + rotate, event.getY() + rotate);
+                    }
+                    currentBox.setCurrent(current);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_POINTER_UP:         // отпускание всех пальцев кроме последнего
+                rotate = 0;
+                currentIndex = 0;
+                break;
+            case MotionEvent.ACTION_UP:                 // отпускание последнего пальца
+                action = "ACTION_UP";
+                currentBox = null;
+                break;
+            case MotionEvent.ACTION_CANCEL:            // сбой
+                action = "ACTION_CANCEL";
+                currentBox = null;
+                break;
+        }
+
+        Log.i(TAG, action + "at x=" + current.x + ", y=" + current.y);
+        return true;
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         // заполнение фона
         canvas.drawPaint(backgroundPaint);
@@ -50,41 +98,11 @@ public class BoxDrawingView extends View {
             float right = Math.max(box.getOrigin().x, box.getCurrent().x);
             float top = Math.min(box.getOrigin().y, box.getCurrent().y);
             float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            if (rotate == 0) {
+                canvas.rotate(rotate, left, bottom);
+            }
             canvas.drawRect(left, top, right, bottom, boxPaint);
         }
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        PointF current = new PointF(event.getX(), event.getY());
-        String action = "";
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                action = "ACTION_DOWN";
-
-                // сброс текущего состояния
-                currentBox = new Box(current);
-                boxen.add(currentBox);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                action = "ACTION_MOVE";
-                if (currentBox != null) {
-                    currentBox.setCurrent(current);
-                    invalidate();
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                action = "ACTION_UP";
-                currentBox = null;
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                action = "ACTION_CANCEL";
-                currentBox = null;
-                break;
-        }
-
-        Log.i(TAG, action + "at x=" + current.x + ", y=" + current.y);
-        return true;
     }
 }
